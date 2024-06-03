7b9d093ce3443f3faa37c05a19ab8b677e855c46
Victoria Suarez <victoriasuarez97@gmail.com>
2024-06-02 21:06:45 -0300

    fix: mobile view in chatbox

--- chatbot/src/app/components/ChatBox/ChatBox.tsx
+++ chatbot/src/app/components/ChatBox/ChatBox.tsx
@@ -1,87 +1,99 @@
-'use client'
+"use client";
 
 import { LegacyRef, useEffect, useRef } from "react";
-import { Avatar, Box, Flex, IconButton, Text, Toast } from "gestalt"
-import { useChat } from 'ai/react';
+import { Avatar, Box, Flex, IconButton, Text, Toast } from "gestalt";
+import { useChat } from "ai/react";
 import { ChatBoxType } from "./types";
 
-import 'gestalt/dist/gestalt.css';
-import './style.css'
+import "gestalt/dist/gestalt.css";
+import "./style.css";
 
 export const ChatBox: ChatBoxType = ({ user }) => {
-    const ref = useRef<HTMLDivElement | null | undefined>()
+	const ref = useRef<HTMLDivElement | null | undefined>();
 
-    const { messages, input, handleInputChange, handleSubmit, error } = useChat({
-        initialMessages: [
-            {
-                id: '1',
-                role: 'assistant',
-                content: `Hello ${user} =^.^= I'm your catssistant!`
-            }
-        ],
-    })
+	const { messages, input, handleInputChange, handleSubmit, error } = useChat({
+		initialMessages: [
+			{
+				id: "1",
+				role: "assistant",
+				content: `Hello ${user} =^.^= I'm your catssistant!`,
+			},
+		],
+	});
 
-    const scrollToBottom = () => {
-        const { offsetHeight, scrollHeight, scrollTop } = ref.current as HTMLDivElement
-        if (scrollHeight <= scrollTop + offsetHeight + 200) {
-            ref.current?.scrollTo(0, scrollHeight)
-        }
-    }
+	const scrollToBottom = () => {
+		const { offsetHeight, scrollHeight, scrollTop } =
+			ref.current as HTMLDivElement;
+		if (scrollHeight <= scrollTop + offsetHeight + 200) {
+			ref.current?.scrollTo(0, scrollHeight);
+		}
+	};
 
-    useEffect(() => {
-        scrollToBottom()
-    }, [messages])
+	useEffect(() => {
+		scrollToBottom();
+	}, [messages]);
 
-    if (error) return (   
-        <Flex
-            justifyContent="center"
-            alignItems="center"
-            width="100%"
-            height="100%"
-        >
-            <Toast
-                type="error"
-                text="Something went wrong. Please try again later."
-                dismissButton={{ onDismiss: () => window.location.reload() }}
-            />
-        </Flex>
-    )
+	if (error)
+		return (
+			<Flex
+				justifyContent="center"
+				alignItems="center"
+				width="100%"
+				height="100%"
+			>
+				<Toast
+					type="error"
+					text="Something went wrong. Please try again later."
+					dismissButton={{ onDismiss: () => window.location.reload() }}
+				/>
+			</Flex>
+		);
 
-    return (
-        <Box borderStyle="sm" rounding={3} padding={6} margin={6} id='wrapper'>
-            <Box ref={ref as LegacyRef<HTMLDivElement>} id='chatContainer'>
-                {
-                    messages.map(({ id, role, content }) => (
-                        <Box key={id} marginTop={4} marginBottom={4}>
-                            <Flex alignItems="center" wrap={false} width='100%'>
-                                <Box>
-                                    {role === 'user'
-                                        ? <Avatar name={user} size='md' />
-                                        : <Avatar src='/robot.png' name='Catbot' verified size='md' />
-                                    }
-                                </Box>
-                                <Box marginStart={3}>
-                                    <Text weight='bold'>{role === 'user' ? user : 'Catbot'}</Text>
-                                    <Text>{content}</Text>
-                                </Box>
-                            </Flex>
-                        </Box>
-                    ))
-                }
-            </Box>
-            <form onSubmit={handleSubmit}>
-                <Flex alignItems="center" wrap={false} width='100%'>
-                    <label htmlFor="userMessage" />
-                    <input
-                        id='userMessage'
-                        onChange={handleInputChange}
-                        value={input}
-                        placeholder="Ask me anything"
-                        autoComplete="off"
-                    />
-                    <IconButton icon="send" accessibilityLabel="Send message" type="submit" />
-                </Flex>
-            </form>
-        </Box>
-    )
-}
+	return (
+		<Box
+			borderStyle="sm"
+			rounding={3}
+			padding={6}
+			margin={6}
+			id="wrapper"
+			minWidth="350px"
+		>
+			<Box ref={ref as LegacyRef<HTMLDivElement>} id="chatContainer">
+				{messages.map(({ id, role, content }) => (
+					<Box key={id} marginTop={4} marginBottom={4}>
+						<Flex alignItems="center" wrap={false} width="100%">
+							<Box>
+								{role === "user" ? (
+									<Avatar name={user} size="md" />
+								) : (
+									<Avatar src="/robot.png" name="Catbot" verified size="md" />
+								)}
+							</Box>
+							<Box marginStart={3}>
+								<Text weight="bold">{role === "user" ? user : "Catbot"}</Text>
+								<Text>{content}</Text>
+							</Box>
+						</Flex>
+					</Box>
+				))}
+			</Box>
+			<form onSubmit={handleSubmit}>
+				<Flex alignItems="center" wrap={false} width="100%">
+					<label htmlFor="userMessage" />
+					<input
+						id="userMessage"
+						onChange={handleInputChange}
+						value={input}
+						placeholder="Ask me anything"
+						autoComplete="off"
+					/>
+					<IconButton
+						icon="send"
+						accessibilityLabel="Send message"
+						type="submit"
+					/>
+				</Flex>
+			</form>
+		</Box>
+	);
+};
